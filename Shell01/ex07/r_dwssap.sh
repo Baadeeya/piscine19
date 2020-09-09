8085951b4214bc59aaef9d1c74327f2e93edc897
David Gutin <dgutin@shi-r1-p4.s19.be>
2020-09-09 14:29:39 +0200

    Bit more of r_dwssap.sh

--- Shell01/ex07/r_dwssap.sh
+++ Shell01/ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | grep -v '#' | awk 'NR%2==0' | cut -d : -f 1 | rev | sort -r
+cat /etc/passwd | grep -v '#' | awk 'NR%2==0' | rev | grep -o '[^:]*$' | sort -r | awk -v line_one="$FT_LINE1" -v line_two="$FT_LINE2" 'NR==line_one,NR==line_two'
