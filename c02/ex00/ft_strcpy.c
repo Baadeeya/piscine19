c17259ff79ffadeff99235028a8397cba6cf3346
David Gutin <dgutin@shi-r1-p4.s19.be>
2020-09-11 20:29:29 +0200

    Ouais a reprendre

--- c02/ex00/ft_strcpy.c
+++ c02/ex00/ft_strcpy.c
@@ -6,47 +6,28 @@
 /*   By: dgutin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/11 14:17:16 by dgutin            #+#    #+#             */
-/*   Updated: 2020/09/11 15:59:08 by dgutin           ###   ########.fr       */
+/*   Updated: 2020/09/11 20:12:06 by dgutin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putstr(char *str)
-{
-	int i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		ft_putchar(str[i]);
-		i++;
-	}
-}
+#include <stdlib.h>
+#include <stdio.h>
 
 char	*ft_strcpy(char *dest, char *src)
 {
-	char *strtemp = dest;
-	if ((dest != NULL) && (src != NULL))
-			{
-				while (*src != NULL)
-				{
-					*dest++ = src++;
-				}
-			}
+	int i;
+
+	i = -1;
+	while (src[i++])
+	{
+		dest[i] = src[i]
+	}
+	return (dest);
 }
 
 int	main(void)
 {
-	char *dest = "Collage";
-	char *src = "pipou";
-
-	*ft_strcpy(dest, src);
-	ft_putstr(*dest);
-	return (0);
-}
+	char *dest;
+	char *src;
+	
