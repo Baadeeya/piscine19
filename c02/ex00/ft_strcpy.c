d0ef70b41414936e4caa26b79f58b75caf03ac0c
David Gutin <dgutin@shi-r1-p4.s19.be>
2020-09-11 14:43:07 +0200

    Update

--- c02/ex00/ft_strcpy.c
+++ c02/ex00/ft_strcpy.c
@@ -6,12 +6,48 @@
 /*   By: dgutin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/11 14:17:16 by dgutin            #+#    #+#             */
-/*   Updated: 2020/09/11 14:19:34 by dgutin           ###   ########.fr       */
+/*   Updated: 2020/09/11 14:42:56 by dgutin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
 char	*ft_strcpy(char *dest, char *src)
 {
+	int i;
 
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
+
+int	main(void)
+{
+	char *dest = "Collage";
+	char *src = "pipou";
+
+	*ft_strcpy(dest, src);
+	ft_putstr(*dest);
+	return (0);
+}
