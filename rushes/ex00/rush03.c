d98eec657a34d1f89288fef099db784dac529e49
David Gutin <dgutin@shi-r1-p4.s19.be>
2020-09-12 16:36:09 +0200

    Fonctionnel normalement

--- rushes/ex00/rush03.c
+++ rushes/ex00/rush03.c
@@ -6,7 +6,7 @@
 /*   By: dgutin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/12 09:44:36 by dgutin            #+#    #+#             */
-/*   Updated: 2020/09/12 15:30:39 by dgutin           ###   ########.fr       */
+/*   Updated: 2020/09/12 16:35:02 by dgutin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,25 +19,48 @@ void	ft_putchar(char c)
 
 void	firstline(int x, int y, int i, int j, int k)
 {
-	if ((i == 1) && (j == 1))
+	if (((i == 1) && (j == 1)) || (j == y))
 	{
 		ft_putchar('A');
 		if (x == 1)
-			y++;
+		{
+			ft_putchar('\n');
+		}
 		i++;
 	}
-	else if (i == x)
+	if ((i != 1) && (i != x))
 	{
-		ft_putchar('C');
-		i = 1;
-		j++;
-	}
-	else
-		while (i < k)
+		while (i <= k)
 		{
 			ft_putchar('B');
 			i++;
 		}
+		if (i == x)
+		{
+			ft_putchar('C');
+			if (j == 1)
+				ft_putchar('\n');
+		}
+	}
+}
+
+void	secondline(int x, int y, int i, int j, int k)
+{
+	k = y - 1;
+	while (j < k)
+	{
+		i = 1;
+		ft_putchar('B');
+		while (i < x - 1)
+		{
+			ft_putchar(' ');
+			i++;
+		}
+		if (x != 1)
+			ft_putchar('B');
+		ft_putchar('\n');
+		j++;
+	}
 }
 
 void	rush(int x, int y)
@@ -48,11 +71,35 @@ void	rush(int x, int y)
 
 	i = 1;
 	j = 1;
-	k = x - 1;
-	if ((x > 0) || (y > 0))
+	if ((x > 0) && (y > 0))
 	{
-		while (j < y++)
+		while (j < y + 1)
 		{
-			firstline(x, y, i, j, k);
-
+			k = x - 1;
+			if (j == 1)
+			{
+				firstline(x, y, i, j, k);
+				j++;
+			}
+			i = 1;
+			if (y > 2)
+			{
+				secondline(x, y, i, j, k);
+				j++;
+			}
+			i = 1;
+			if (j == y)
+			{
+				k = x - 1;
+				firstline(x, y, i, j, k);
+				j++;
+			}
 		}
+	}
+}
+
+int	main(void)
+{
+	rush(5,5);
+	return (0);
+}
