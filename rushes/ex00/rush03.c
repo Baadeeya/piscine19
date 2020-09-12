4f868944af9a4dfaf7f4bd1c6c8f6f178df7d0ba
David Gutin <dgutin@shi-r1-p4.s19.be>
2020-09-12 11:01:23 +0200

    Correct ft_print_numbers

--- rushes/ex00/rush03.c
+++ rushes/ex00/rush03.c
@@ -1,18 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*   rush03.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dgutin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/09 20:12:29 by dgutin            #+#    #+#             */
-/*   Updated: 2020/09/12 11:00:56 by dgutin           ###   ########.fr       */
+/*   Created: 2020/09/12 09:44:36 by dgutin            #+#    #+#             */
+/*   Updated: 2020/09/12 10:15:40 by dgutin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_print_numbers(void)
+void	ft_putchar(char c);
+
+void	rush(int x, int y)
 {
-	write(1, "0123456789", 10);
-}
+	char A;
+	char B;
+	char C;
+	int i;
+	char *tab1;
+	char *tab2;
+
+	A = 'A';
+	B = 'B';
+	C = 'C';
+	i = 0;
+	*tab1 = '\0';
+	*tab2 = '\0';
+	if ((x > 0) || (y > 0))
+	{
+		while (tab2[i++] != '\0')
+		{
+			while (tab1[i++] != '\0')
+			{
+				ft_putchar('A');
+				 if (x > 1)
+			}
