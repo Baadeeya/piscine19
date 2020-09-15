47547dc35ac379689aed3cc4d05f0616a7461e57
David Gutin <dgutin@shi-r1-p4.s19.be>
2020-09-15 10:06:22 +0200

    fix

--- c02/ex01/ft_strncpy.c
+++ c02/ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: dgutin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/13 20:18:37 by dgutin            #+#    #+#             */
-/*   Updated: 2020/09/15 09:33:06 by dgutin           ###   ########.fr       */
+/*   Updated: 2020/09/15 10:06:03 by dgutin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,11 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	while ((i < n) && (src[i]))
 	{
 		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
 		i++;
 	}
 	return (dest);
