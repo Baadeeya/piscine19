92e7beb40b7a1c14c555dab6b13f2e6f7019c7e3
David Gutin <dgutin@shi-r1-p4.s19.be>
2020-09-15 11:53:01 +0200

    Correct c02 ex04 & str cmps

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -6,22 +6,16 @@
 /*   By: dgutin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/14 18:22:34 by dgutin            #+#    #+#             */
-/*   Updated: 2020/09/14 18:39:02 by dgutin           ###   ########.fr       */
+/*   Updated: 2020/09/15 10:59:34 by dgutin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i;
+	int i;
 
 	i = 0;
-	if (n == 0)
-		return (0);
-	while ((s1[i] == s2[i]) && (i < n - 1))
-	{
-		if (s1[i] == '\0')
-			return (0);
+	while ((s1[i] == s2[i]) && s1[i])
 		i++;
-	}
 	return (s1[i] - s2[i]);
 }
