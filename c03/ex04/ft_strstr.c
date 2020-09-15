f613be6ae3397a1034f984889a1fc9c400ecfd46
David Gutin <dgutin@shi-r1-p4.s19.be>
2020-09-15 17:23:45 +0200

    strstrs.c

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -6,32 +6,30 @@
 /*   By: dgutin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/15 11:59:58 by dgutin            #+#    #+#             */
-/*   Updated: 2020/09/15 12:44:15 by dgutin           ###   ########.fr       */
+/*   Updated: 2020/09/15 15:37:59 by dgutin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strstr(char *str, char *to_find)
 {
-	int i;
-	int x;
+	char *aiguille;
+	char *foin;
 
-	i = 0;
-	x = 0;
+	aiguille = to_find;
+	foin = str;
 	if (*to_find == 0)
 		return (str);
-	while (str[i])
+	while (1)
 	{
-		if (str[i] != to_find[x])
-			i++;
-		if (str[i] == to_find[x])
-		{
-			while ((to_find[x]) && (str[i + 1] == to_find[x + 1]))
-				{
-					x++;
-					i++;
-				}
-			if str[i + 1] ?????????????????
-		}
-		i++;
+		if (*aiguille == '\0')
+			return ((char *)(foin - (aiguille - to_find)));
+		if (*foin == *aiguille)
+			aiguille++;
+		else
+			aiguille = to_find;
+		if (*foin == '\0')
+			break ;
+		foin++;
 	}
+	return (0);
 }
