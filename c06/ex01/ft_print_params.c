bdd09922778485f7d46df2012e9fc516e2989f44
David Gutin <dgutin@shi-r1-p4.s19.be>
2020-09-17 16:12:39 +0200

    ft_print_params.c

--- c06/ex01/ft_print_params.c
+++ c06/ex01/ft_print_params.c
@@ -6,10 +6,32 @@
 /*   By: dgutin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/17 15:50:14 by dgutin            #+#    #+#             */
-/*   Updated: 2020/09/17 15:50:28 by dgutin           ###   ########.fr       */
+/*   Updated: 2020/09/17 16:12:06 by dgutin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		ft_putchar(*str++);
+}
+
+int		main(int argc, char **argv)
+{
+	int i;
+
+	i = 1;
+	while (argc > i)
+	{
+		ft_putstr(argv[i]);
+		ft_putchar('\n');
+		i++;
+	}
+}
