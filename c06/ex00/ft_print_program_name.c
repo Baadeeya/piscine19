c366d36141d19b186fb9da53cd03cb95a86f62ab
David Gutin <dgutin@shi-r1-p4.s19.be>
2020-09-17 15:51:28 +0200

    argc argv

---
+++ c06/ex00/ft_print_program_name.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgutin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/17 15:39:59 by dgutin            #+#    #+#             */
+/*   Updated: 2020/09/17 15:44:55 by dgutin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		ft_putchar(*str++);
+}
+
+int		main(int argc, char **argv)
+{
+	if (argc)
+	{
+		ft_putstr(argv[0]);
+		ft_putchar('\n');
+	}
+	return (0);
+}
