384521f6d270fb5637d6fa889f7bb0cfe7b82277
Baadeeya <david14av@gmai.com>
2020-09-18 16:12:12 +0200

    find next prime

--- c05/ex07/ft_find_next_prime.c
+++ c05/ex07/ft_find_next_prime.c
@@ -12,40 +12,25 @@
 
 #include <stdio.h>
 
-int	ft_is_prime(int nb)
-{
-	int i;
-
-	i = 2;
-	if (nb < 2)
-		return (0);
-	while (i < nb)
-	{
-		if (nb % i == 0)
-			return (0);
-		else
-			i++;
-	}
-	return (1);
-}
-
 int	ft_find_next_prime(int nb)
 {
-	int i;
-	int x;
+	unsigned int i;
 
-	i = 2;
-	x = nb;
-	if (ft_is_prime(nb) == 1)
-		return (nb);
-	else
-	{
+	if (nb < 3)
+		return (2);
+	if (!(nb % 2))
 		nb++;
-		while (nb % i > 1)
-			i++
-		return (nb);
+	i = 3;
+	while (i * i <= (unsigned int)nb)
+	{
+		if (!(nb % i))
+		{
+			nb += 2;
+			i = 1;
+		}
+		i += 2;
 	}
-	return (0);
+	return (nb);
 }
 
 int	main(void)
