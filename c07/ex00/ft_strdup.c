7ee011f55e2aa8bf7ca2c328228d860f0df31d0b
David Gutin <dgutin@shi-r5-p3.s19.be>
2020-09-19 22:37:30 +0200

    start c07

---
+++ c07/ex00/ft_strdup.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgutin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/19 19:47:57 by dgutin            #+#    #+#             */
+/*   Updated: 2020/09/19 19:56:08 by dgutin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strdup(char *src)
+{
+	char *dest;
+
+	dest = malloc((ft_strlen(src) + 1) * sizeof(char));
+	if (dest == '\0')
+		return (NULL);
+	return (ft_strcpy(dest, src));
+}
