3177aa15e81e120f5c24dbda77c1454abbbf130d
David Gutin <dgutin@shi-r3-p1.s19.be>
2020-09-22 21:14:01 +0200

    Converted base_from to int in decimal

---
+++ c07/ex04/ft_convert_base.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_convert_base.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dgutin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/22 17:26:23 by dgutin            #+#    #+#             */
+/*   Updated: 2020/09/22 21:13:41 by dgutin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_base(char c, char *base)
+{
+	int i;
+
+	i = -1;
+	while (base[++i])
+		if (c == base[i])
+			return (i);
+	return (-1);
+}
+
+int	ft_strlen_atoi(char *str)
+{
+	int i;
+
+	i = -1;
+	while (str[++i])
+	{
+		if (str[i] == '+' || str[i] == '-'
+				|| str[i] == ' ' || (str[i] >= 9 && str[i] <= 13)
+				|| ft_base(str[i], str + i + 1) >= 0)
+			return (0);
+	}
+	return (i);
+}
+
+int	ft_atoi_base(char *str, char *base)
+{
+	int i;
+	int nbr;
+	int len;
+	int n;
+
+	if ((len = ft_strlen_atoi(base)) < 2)
+		return (0);
+	while ((*str >= 9 && *str <= 13) || *str == ' ')
+		str++;
+	n = 0;
+	while (*str == '-' || *str == '+')
+		if (*str++ == '-')
+			n = 1 - n;
+	nbr = 0;
+	while ((i = ft_base(*str, base)) >= 0)
+	{
+		nbr = nbr * len + i;
+		str++;
+	}
+	if (n)
+		nbr *= -1;
+	return (nbr);
+}
+
+char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
+{
+	int	nb;
+
+	nb = ft_atoi_base(nbr, base_from);
+}
