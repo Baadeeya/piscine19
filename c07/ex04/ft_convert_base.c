7d83514fe1ab5b50b298c646769d96727f210710
David Gutin <david14av@gmail.com>
2020-09-23 11:18:18 +0200

    Almost done

--- c07/ex04/ft_convert_base.c
+++ c07/ex04/ft_convert_base.c
@@ -6,13 +6,15 @@
 /*   By: dgutin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/22 17:26:23 by dgutin            #+#    #+#             */
-/*   Updated: 2020/09/22 22:22:56 by dgutin           ###   ########.fr       */
+/*   Updated: 2020/09/23 11:18:10 by dgutin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
 int		ft_base(char c, char *base)
 {
-	int i;
+	int 			i;
 
 	i = -1;
 	while (base[++i])
@@ -21,16 +23,16 @@ int		ft_base(char c, char *base)
 	return (-1);
 }
 
-int		ft_strlen_atoi(char *str)
+int		ft_strlen_base(char *base)
 {
-	int i;
+	int 			i;
 
 	i = -1;
-	while (str[++i])
+	while (base[++i])
 	{
-		if (str[i] == '+' || str[i] == '-'
-				|| str[i] == ' ' || (str[i] >= 9 && str[i] <= 13)
-				|| ft_base(str[i], str + i + 1) >= 0)
+		if (base[i] == '+' || base[i] == '-'
+				|| base[i] == ' ' || (base[i] >= 9 && base[i] <= 13)
+				|| ft_base(base[i], base + i + 1) >= 0)
 			return (0);
 	}
 	if (i < 2)
@@ -40,13 +42,13 @@ int		ft_strlen_atoi(char *str)
 
 int		ft_atoi_base(char *str, char *base)
 {
-	int i;
-	int nbr;
-	int len;
-	int n;
+	int 			i;
+	int 			nbr;
+	int 			len;
+	int 			n;
 
 	if ((len = ft_strlen_atoi(base)) < 2)
-		return (0);
+		return (NULL);
 	while ((*str >= 9 && *str <= 13) || *str == ' ')
 		str++;
 	n = 0;
@@ -71,13 +73,49 @@ int		ft_nbrlen(unsigned int n, unsigned int base_size)
 	return (1 + ft_nbrl(n / base_size, base_size));
 }
 
+void	ft_itoa_base(int nbr, int baselen)
+{
+	int 			i;
+	int				neg;
+	int				modulo;
+	char			*dest;
+
+	i = 0;
+	if (nbr == 0)
+	{
+		dest[i++] = '0';
+		dest[i] = '\0';
+		return (dest);
+	}
+	if (nbr < 0)
+	{
+		neg = 1;
+		nbr = -nbr;
+	}
+	while (nbr != 0)
+	{
+		modulo = nbr % baselen;
+		str[i++] = (modulo > 9) ? (modulo - 10) + 'a' : rem + '0';
+		nbr = nbr/baselen;
+	}
+
+}
+
 char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 {
 	char			*dest;
 	unsigned int	nb;
 	int				size;
 	int				i;
-	int				n;
+	int				fromlen;
+	int				tolen;
 
-	n = ft_base(base_from);
+	if (!(fromlen = ft_strlen_base(base_from)))
+		return (NULL);
+	if (!(tolen = ft_strlen_base(base_to)))
+		return (NULL);
+	if (!(dest = malloc(sizeof(char)) * baselen))
+		return (NULL);
+	if ((dest = ft_itoa_base(from, tolen)) < 2)
+		return (NULL);
 }
