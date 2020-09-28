603f99b6156c4b52f53199be7d5f0af5db1fb867
David Gutin <david14av@gmail.com>
2020-09-28 19:17:44 +0200

    ft_is_sort fix

--- c11/ex04/ft_is_sort.c
+++ c11/ex04/ft_is_sort.c
@@ -6,33 +6,20 @@
 /*   By: dgutin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/28 14:57:57 by dgutin            #+#    #+#             */
-/*   Updated: 2020/09/28 15:12:40 by dgutin           ###   ########.fr       */
+/*   Updated: 2020/09/28 19:09:07 by dgutin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_is_sort(int *tab, int length, int (*f)(int, int))
 {
 	int	i;
-	int	x;
 
 	i = 0;
-	x = 1;
-	while (i < length - 1 && x)
+	while (i + 1 < length)
 	{
-		if ((*f)(tab[i], tab[i + 1]) < 0)
-			x = 0;
-		i++;
-	}
-	if (x != 1)
-	{
-		x = 1;
-		i = 0;
-		while (i < length - 1)
-		{
-			if ((*f)(tab[i], tab[i + 1]) > 0)
-				return (0);
-			i++;
-		}
+		if (f(tab[i], tab[i + 1]) > 0)
+			return (0);
+		++i;
 	}
 	return (1);
 }
