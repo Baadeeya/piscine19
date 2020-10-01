9ff5aa8b8b51ec39f0dbc5448d0eff8c7297205e
David Gutin <david14av@gmail.com>
2020-10-01 16:10:29 +0200

    Improve c06ex03

--- c06/ex03/ft_sort_params.c
+++ c06/ex03/ft_sort_params.c
@@ -5,55 +5,75 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dgutin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/09/20 18:08:49 by dgutin            #+#    #+#             */
-/*   Updated: 2020/09/21 16:01:44 by dgutin           ###   ########.fr       */
+/*   Created: 2020/10/01 16:06:11 by dgutin            #+#    #+#             */
+/*   Updated: 2020/10/01 16:10:03 by dgutin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int			ft_strcmp(char *s1, char *s2)
+int		ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+void	ft_display(int ac, char **av)
 {
 	int i;
 
-	i = -1;
-	while (s1[++i] || s2[++i])
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-	return (0);
-}
-
-void		ft_putstr(char *c)
-{
-	while (*c)
-		write(1, c++, 1);
-	write(1, "\n", 1);
-}
-
-int			main(int argc, char **argv)
-{
-	char	*(ar[argc]);
-	char	*tmp;
-	int		i;
-	int		o;
-
-	i = 0;
-	while (++i < argc)
-		ar[i] = argv[i];
-	i = 0;
-	while (++i < argc)
+	i = 1;
+	while (i < ac)
 	{
-		o = i;
-		while (++o < argc)
-			if (ft_strcmp(ar[i], ar[o]) > 0)
-			{
-				tmp = ar[i];
-				ar[i] = ar[o];
-				ar[o] = tmp;
-			}
+		ft_putstr(av[i]);
+		ft_putchar('\n');
+		i++;
 	}
-	i = 0;
-	while (++i < argc)
-		ft_putstr(ar[i]);
+}
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 && s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	if (!*s1)
+		return (0);
+	return (*s1 - *s2);
+}
+
+int		main(int ac, char **av)
+{
+	int		i;
+	int		x;
+	char	*tmp;
+
+	i = 1;
+	x = 1;
+	while (i < ac)
+	{
+		x = 1;
+		while (x < ac - 1)
+		{
+			if (ft_strcmp(av[x], av[x + 1]) > 0)
+			{
+				tmp = av[x];
+				av[x] = av[x + 1];
+				av[x + 1] = tmp;
+			}
+			x++;
+		}
+		i++;
+	}
+	ft_display(ac, av);
 	return (0);
 }
